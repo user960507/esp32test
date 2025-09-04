50e9c3f3cbdc2388242179b4dd479bc804920acc
user960507 <agrodriguez@protonmail.com>
2025-09-04 17:13:15 +0200

    initial commit

---
+++ main/ledc_basic_example_main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+// Define GPIO pin for LED
+#define LED_BUILTIN 8
+
+void led_setup(void){
+    // Configure GPIO
+    gpio_reset_pin(LED_BUILTIN);
+    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
+}
+
+void blink_code(int cantidad, int tiempoms, int ciclos)
+{
+    for(int i = 0; i < ciclos; i++){
+        //ciclos
+        for(int j = 0; j <cantidad; j++)
+        {
+            //cantidad de blinks
+            
+                // Turn LED ON
+                printf("LED ON\n");
+                gpio_set_level(LED_BUILTIN, 1);
+                vTaskDelay(tiempoms / portTICK_PERIOD_MS); // Delay 1 second
+
+                // Turn LED OFF
+                printf("LED OFF\n");
+                gpio_set_level(LED_BUILTIN, 0);
+                vTaskDelay(tiempoms / portTICK_PERIOD_MS); // Delay 1 second
+        }
+        gpio_set_level(LED_BUILTIN, 0);
+        vTaskDelay(5*tiempoms / portTICK_PERIOD_MS); // Delay 5 second
+    }
+}
+
+void app_main(void)
+{
+
+    led_setup();
+
+    blink_code(3,1000,5);
+
+}
