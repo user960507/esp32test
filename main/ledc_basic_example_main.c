4cb594ed855d12cf5f71649c4262c31b03e800ea
user960507 <agrodriguez@protonmail.com>
2025-09-09 10:33:59 +0200

    adding temp sensor config

--- main/ledc_basic_example_main.c
+++ main/ledc_basic_example_main.c
@@ -2,10 +2,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/temperature_sensor.h"
+
 
 // Define GPIO pin for LED
 #define LED_BUILTIN 8 // recuerda que en esp 0 es on para el led
 
+
+//led functions and subs
 void led_setup(void){
     // Configure GPIO
     gpio_reset_pin(LED_BUILTIN);
@@ -13,7 +17,8 @@ void led_setup(void){
 }
 
 void blink_code(int cantidad, int tiempoms, int ciclos)
-{
+{   
+
     for(int i = 0; i < ciclos; i++){
         //ciclos
         for(int j = 0; j <cantidad; j++)
@@ -22,6 +27,7 @@ void blink_code(int cantidad, int tiempoms, int ciclos)
             
                 // Turn LED ON
                 printf("LED ON\n");
+
                 gpio_set_level(LED_BUILTIN, 0);
                 vTaskDelay(tiempoms / portTICK_PERIOD_MS); // Delay 1 second
 
@@ -30,16 +36,36 @@ void blink_code(int cantidad, int tiempoms, int ciclos)
                 gpio_set_level(LED_BUILTIN, 1);
                 vTaskDelay(tiempoms / portTICK_PERIOD_MS); // Delay 1 second
         }
-        gpio_set_level(LED_BUILTIN, 1);
+        gpio_set_level(LED_BUILTIN, 0);
         vTaskDelay(5*tiempoms / portTICK_PERIOD_MS); // Delay 5 second
     }
+
+
+}
+
+//temp sensor functions and subs
+temperature_sensor_handle_t temp_setup(void){
+    temperature_sensor_handle_t temp_sensor = NULL;
+    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(10, 50);
+    temperature_sensor_install(&temp_sensor_config, &temp_sensor);
+    return temp_sensor;
 }
 
 void app_main(void)
-{
+{   
+    //variables
+    temperature_sensor_handle_t temp_sensor;
+    float temperature;
 
+    //setups
     led_setup();
 
-    blink_code(3,1000,5);
+    temp_sensor = temp_setup();
+    temperature_sensor_enable(temp_sensor);
+
+    //main logic
+    temperature_sensor_get_celsius(temp_sensor, &temperature);
+    printf("lectura %.02f C", temperature);
+    blink_code(5,500,5);
 
 }
